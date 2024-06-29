90b4aefb2346e7530fdcac77544019845c650d93
BenMoreira <bmoreira@ggc.edu>
2024-06-29 16:02:22 -0400

    Adds envelope and card coming out of it animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { motion } from 'framer-motion';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [flip, setFlip] = useState(true);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-full min-h-screen bg-gray-400'>
+        <div className='w-full h-screen flex flex-col justify-center items-center'>
+          <div className='w-[360px] h-[300px]' onClick={() => setFlip(!flip)}>
+            <motion.div className='border-l-[180px] border-l-transparent border-r-[180px] border-r-transparent border-t-[150px] 
+              border-t-black rounded-xl absolute'
+                
+              transition={{duration: 0.7}}
+              animate={{rotateX: flip ? 0 : 180, y: flip ? 0 : -140, opacity: flip ? 1 : 0}}
+            ></motion.div>
+
+            <motion.div className='border-t-[150px] border-t-transparent border-b-[180px] border-b-transparent border-l-[180px] 
+            border-l-red-600 rounded-xl absolute' animate={{y: 0, opacity: flip ? 1 : 0}}></motion.div>
+
+            <motion.div className='border-l-[180px] border-l-transparent border-r-[180px] border-r-transparent border-b-[180px] 
+            border-b-red-600 rounded-xl absolute' animate={{y: 150, x: 0, opacity: flip ? 1 : 0}}></motion.div>
+
+            <motion.div className='border-t-[150px] border-t-transparent border-b-[180px] border-b-transparent border-r-[180px] 
+            border-r-red-600 rounded-xl absolute' animate={{y: 0, x: 180, opacity: flip ? 1 : 0}}></motion.div>
+
+            <motion.div className='w-[360px] h-[500px] bg-pink-300 border-pink-300 rounded-xl'
+              transition={{duration: 1.5}}
+              initial={{opacity: 0}}
+              animate={{opacity: flip ? 0 : 1, y: flip ? 0 : [-100, -400, -100]}}
+            >
+              Hello!!!!!
+            </motion.div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
