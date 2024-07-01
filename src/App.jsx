73442fb5730cee44d919db6e417945dc91658800
BenMoreira <bmoreira@ggc.edu>
2024-07-01 13:43:05 -0400

    Adds imgs and fixes look of card

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import { motion } from 'framer-motion';
 import ConfettiExplosion from 'react-confetti-explosion';
 import antbd from './assets/antbd.png';
+import bgbd from './assets/bgbd.jpg';
 import './App.css'
 
 function App() {
@@ -23,15 +24,15 @@ function App() {
             ></motion.div>
 
             <motion.div className='border-t-[150px] border-t-transparent border-b-[180px] border-b-transparent border-l-[180px] 
-            border-l-red-600 rounded-xl absolute' transition={{duration: 0.7}} animate={{y: 0, opacity: flip ? 1 : 0}}></motion.div>
+            border-l-pink-400 rounded-xl absolute' transition={{duration: 0.7}} animate={{y: 0, opacity: flip ? 1 : 0}}></motion.div>
 
             <motion.div className='border-l-[180px] border-l-transparent border-r-[180px] border-r-transparent border-b-[180px] 
-            border-b-red-600 rounded-xl absolute' transition={{duration: 0.7}} animate={{y: 150, x: 0, opacity: flip ? 1 : 0}}></motion.div>
+            border-b-pink-400 rounded-xl absolute' transition={{duration: 0.7}} animate={{y: 150, x: 0, opacity: flip ? 1 : 0}}></motion.div>
 
             <motion.div className='border-t-[150px] border-t-transparent border-b-[180px] border-b-transparent border-r-[180px] 
-            border-r-red-600 rounded-xl absolute' transition={{duration: 0.7}} animate={{y: 0, x: 180, opacity: flip ? 1 : 0}}></motion.div>
+            border-r-pink-400 rounded-xl absolute' transition={{duration: 0.7}} animate={{y: 0, x: 180, opacity: flip ? 1 : 0}}></motion.div>
 
-            <motion.div className='w-[360px] h-[600px] bg-gradient-to-t from-pink-500 via-pink-300 to-pink-500 border-pink-300 rounded-xl'
+            <motion.div className='w-[360px] h-[600px] bg-[url("src/assets/bgbd.jpg")] bg-cover rounded-xl'
               transition={{duration: 2}}
               initial={{opacity: 0}}
               animate={{opacity: flip ? 0 : 1, y: flip ? 0 : [-135, -400, -135]}}
@@ -40,32 +41,32 @@ function App() {
               {isExploding && <ConfettiExplosion force={0.7} duration={5000} particleCount={300} colors={['#f9a8d4', '#3b82f6', '#d4af37']} />}
               </div>
 
-              <motion.div className='w-full flex flex-col justify-center items-center gap-2 font-bold'>
-                <motion.div className=''>
+              <motion.div className='w-full flex flex-col gap-2 font-bold py-5 px-4'>
+                <motion.div className='text-pink-800 text-2xl py-4 text-center'>
                   El 1er cumpleaños de
                 </motion.div>
 
-                <motion.div className=''>
+                <motion.div className='text-pink-900 text-3xl pb-4 text-center'>
                   Antonella Ulloa
                 </motion.div>
 
-                <motion.div className=''>
+                <motion.div className='text-pink-800 text-lg pb-4 text-right'>
                   Ven a celebrar con ella
                 </motion.div>
 
-                <motion.div className=''>
+                <motion.div className='text-pink-800 text-lg'>
                   El dia Sábado 27 de Julio
                 </motion.div>
 
-                <motion.div className=''>
+                <motion.div className='text-pink-800 text-lg pb-4'>
                   a las 5 P.M.
                 </motion.div>
 
-                <motion.div className=''>
+                <motion.div className='text-pink-800 text-lg pb-4 text-right'>
                   3896 Egypt Rd., Snellville, GA.
                 </motion.div>
 
-                <motion.div>
+                <motion.div className='flex flex-col justify-center items-center pl-3'>
                   <img src={antbd} className='w-[180px]' />
                 </motion.div>
               </motion.div>
